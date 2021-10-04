c311c5b6516b38e28c1ffd520ea228383a0a3f80
s-Nishant <47000389+nishant-salode@users.noreply.github.com>
2021-10-04 11:35:44 +0530

    Create Script__Tag__parsing.js

---
+++ Script__Tag__parsing.js
@@ -0,0 +1,22 @@
+Difference b/w ASYNC, DEFER and normal script tag :
+
+
+
+<script src="https://domain.com"></script>
+
+1. Html parsing pauses once it finds the script tag is received.
+2. Fetches the script.
+3. Executes the script.
+4. HTML parsing is resumed.
+
+<script async src="https://domain.com"></script>
+
+1. Fetches the script asynchronously with HTML parsing.
+2. Once script is fetched it pauses HTML parsing and executes the script.
+3. HTML parsing is resumed.
+
+
+<script defer src="https://domain.com"></script>
+
+1. Fetches the script asynchronously with HTML parsing.
+2. Executes the script once the HTML parsing is completed.
